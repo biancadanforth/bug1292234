ffcae9532227e62c26b7f30e6d44121f36d33ea4
Bianca Danforth <biancadanforth@gmail.com>
2019-12-09 12:08:07 -0800

    Update web-ext-config.js to see if hidden: true in manifest.json shows the addon in about:debugging
    I put a console.log here: https://searchfox.org/mozilla-central/source/devtools/server/actors/descriptors/webextension.js#73-95 in a local build of Firefox to log the addon id and the hidden property. Currently the addon is NOT hidden in about:debugging, though it is hidden in about:addons.

--- test_extensions/1573201-test-extension-3/web-ext-config.js
+++ test_extensions/1573201-test-extension-3/web-ext-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   run: {
-    // Points to my local artifact development build of Firefox
     firefox: '/Users/bdanforth/src/mozilla-unified/objdir-frontend-debug-artifact/dist/Nightly.app/Contents/MacOS/firefox',
+    pref: [
+      'extensions.legacy.enabled=true',
+      'xpinstall.signatures.required=false',
+      'devtools.aboutdebugging.showHiddenAddons=false',
+    ],
     startUrl: [
       'about:debugging#/runtime/this-firefox',
-    ],
-    pref: [
-      'devtools.storage.extensionStorage.enabled=true',
     ],
   },
 };
